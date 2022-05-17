28b77c44989adc4ce1558149f25f31bf92b021e0
darren <dkong1321@gmail.com>
2022-05-17 14:55:48 -0700

    add error validations

--- calendar_this/app/routes.py
+++ calendar_this/app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect
 import os
 import psycopg2
 from app.forms import AppointmentForm
+from datetime import datetime
 CONNECTION_PARAMETERS = {
     "user": os.environ.get("DB_USER"),
     "password": os.environ.get("DB_PASS"),
@@ -12,10 +13,30 @@ bp = Blueprint("main", __name__, url_prefix='/')
 
 
 
-@bp.route("/")
+@bp.route("/", methods=["GET","POST"])
 
 def main():
     form = AppointmentForm()
+    if form.validate_on_submit():
+        params = {
+            'name': form.name.data,
+            'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
+            'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
+            'description': form.description.data,
+            'private': form.private.data
+        }
+        with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+            with conn.cursor() as curs:
+                curs.execute(
+                    """
+                    INSERT INTO appointments (name, start_datetime, end_datetime, description, private)
+                    VALUES (%(name)s, %(start_datetime)s,%(end_datetime)s,%(description)s,%(private)s );
+                    """,
+                    params
+                )
+        return redirect("/")
+
+
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         with conn.cursor() as curs:
             curs.execute(

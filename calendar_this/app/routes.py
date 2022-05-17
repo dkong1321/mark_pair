e7a952530ca2c3e1b21ea7360216bd85dee3caeb
darren <dkong1321@gmail.com>
2022-05-17 13:28:39 -0700

    first route in progress

--- calendar_this/app/routes.py
+++ calendar_this/app/routes.py
@@ -1,7 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect
+import os
+import psycopg2
+CONNECTION_PARAMETERS = {
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASS"),
+    "dbname": os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+}
 bp = Blueprint("main", __name__, url_prefix='/')
+
+
 
 @bp.route("/")
 
 def main():
-    return 'Calendar Working'
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            curs.execute(
+                """
+                SELECT id, name, start_datetime, end_datetime
+                FROM appointments
+                ORDER BY start_datetime;
+                """,
+            )
+            rows = curs.fetchall()
+            print(rows)
+    return render_template("main.html", rows=rows)

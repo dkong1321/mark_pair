57154358a60ea506d6f450ee86cabe7a95c13239
thisismydisplay <markrockwellosman@gmail.com>
2022-05-17 17:49:21 -0500

    error in select sql

--- calendar_this/app/routes.py
+++ calendar_this/app/routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, redirect
+from time import timezone
+from flask import Blueprint, render_template, redirect, url_for
 import os
 import psycopg2
 from app.forms import AppointmentForm
-from datetime import datetime
+from datetime import datetime, timedelta, date, tzinfo
 CONNECTION_PARAMETERS = {
     "user": os.environ.get("DB_USER"),
     "password": os.environ.get("DB_PASS"),
@@ -13,9 +14,14 @@ bp = Blueprint("main", __name__, url_prefix='/')
 
 
 
-@bp.route("/", methods=["GET","POST"])
-
+@bp.route("/")
 def main():
+    d = datetime.now()
+    return redirect(url_for(".daily", year=d.year, month=d.month, day=d.day))
+
+@bp.route("/<int:year>/<int:month>/<int:day>", methods=["GET","POST"])
+
+def daily(year, month, day):
     form = AppointmentForm()
     if form.validate_on_submit():
         params = {
@@ -34,18 +40,30 @@ def main():
                     """,
                     params
                 )
-        return redirect("/")
+        # return redirect("/")
+        return redirect("/<int:year>/<int:month>/<int:day>")
+
+
 
 
     with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
         with conn.cursor() as curs:
+            day = date(year, month, day)
+            day.strftime("%H:%M:%S")
+            td = timedelta(days=1)
+            next_day = day+td
+            print(day)
+            print(td)
+            print(next_day)
             curs.execute(
                 """
                 SELECT id, name, start_datetime, end_datetime
                 FROM appointments
+                WHERE start_datetime BETWEEN %(day)s AND %(next_day)s
                 ORDER BY start_datetime;
-                """,
+                """
             )
             rows = curs.fetchall()
+
 
     return render_template("main.html", rows=rows, form=form)
